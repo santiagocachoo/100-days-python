02cc1fb91546c56a099227d4877a5a2738bcfa68
santiago <a01255853@tec.mx>
2025-01-16 22:09:30 -0700

    dia uno

---
+++ dia_01/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+input_city = input("What's the name of the city you grew up in?\n")
+input_pet_name = input("What´s the name of your childhood pet?\n")
+print(f"Your band name could be {input_city} {input_pet_name}")
