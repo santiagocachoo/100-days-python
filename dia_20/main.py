36215c463f21fc2c244d578a558ab996f07db9a7
santiago <a01255853@tec.mx>
2025-08-07 12:04:41 -0700

    dia 20

---
+++ dia_20/main.py
@@ -0,0 +1,31 @@
+from turtle import Screen, Turtle
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.right, "d")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+screen.exitonclick()
