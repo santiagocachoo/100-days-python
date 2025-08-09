39515da5ba8dbff066ad4c6ea901951bd532c933
santiago <a01255853@tec.mx>
2025-08-09 15:05:56 -0700

    dia 21, terminar snake

--- dia_20/main.py
+++ dia_20/main.py
@@ -1,6 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(600, 600)
@@ -9,6 +11,9 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+scoreboard.update_scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -24,8 +29,25 @@ screen.onkey(snake.right, "d")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
-
+    time.sleep(0.085)
     snake.move()
+
+    # detect collisionn with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False  
+        scoreboard.game_over()
+
+    # detect collision with tail
+    # if head collides with any segment = GAME OVER
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
