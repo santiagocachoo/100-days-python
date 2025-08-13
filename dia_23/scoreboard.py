ef9ca0c0e0bd364318777c06dab56b0597840a82
santiago <a01255853@tec.mx>
2025-08-12 22:13:49 -0700

    dia 23 update scoreboard

--- dia_23/scoreboard.py
+++ dia_23/scoreboard.py
@@ -5,15 +5,15 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
+        self.level = 0
         self.color("black")
         self.hideturtle()
         self.penup()
-        self.goto(0, 270)
+        self.goto(-250, 270)
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Scoreboard: {self.score}", align="center", font=FONT)
+        self.write(f"Level: {self.level}", align="center", font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
@@ -21,5 +21,5 @@ class Scoreboard(Turtle):
 
     def increase_score(self):
         self.clear()
-        self.score += 1
+        self.level += 1
         self.update_scoreboard()
